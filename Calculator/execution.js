11b9be02371ac11a7fbb584915f73c54e43e21f6
Nithya2154 <nithyananthamgunasekar@gmail.com>
2025-08-29 16:47:29 +0530

    Add files via upload

---
+++ Calculator/execution.js
@@ -0,0 +1,7 @@
+var cal = require('./mathcal.js')
+var x = 10
+var y = 5
+console.log("Addition:", cal.add(x, y))
+console.log("Subtraction:", cal.sub(x, y))
+console.log("Multiplication:", cal.mul(x, y))
+console.log("Division:", cal.div(x, y))
